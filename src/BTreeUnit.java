e44750940f19e27137e847352fc8a695c3a2b8d1
Kamil <Why2Man4@gmail.com>
2026-04-24 10:47:42 +0300

    some changes

---
+++ src/BTreeUnit.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BTreeUnit {
+    int key;
+    List<BTreeUnit> childes = new ArrayList<>();
+    List<String> info = null;
+
+    public BTreeUnit(){
+
+    }
+}
